205d9c229e9926e565fd933c65c82465fa040064
Armando Frías <armando_ramirez097@hotmail.com>
2023-12-01 11:42:03 -0600

    User default

--- User_Default/Presentation/Features/HomeVC/HomeVC.swift
+++ User_Default/Presentation/Features/HomeVC/HomeVC.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class HomeVC: UIViewController {
+    
+    private let kMykey = "MY_KEY"
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,6 +18,34 @@ class HomeVC: UIViewController {
         
     }
 
-
+    //Recuperamos preferencias
+    @IBAction func getButtonAction(_ sender: Any) {
+        if let value = UserDefaults.standard.string(forKey: kMykey){
+            showAlert(message: value)
+        } else {
+            showAlert(message: "No hay un valor para esta clave")
+            
+        }
+        
+    }
+    //Guarda preferencias
+    @IBAction func putButtonAction(_ sender: Any) {
+        UserDefaults.standard.set("ARMANDO", forKey: kMykey)
+        UserDefaults.standard.synchronize()
+        showAlert(message: "Hemos guardado un valor")
+    }
+    // Borra preferencias
+    @IBAction func deleteButtonAction(_ sender: Any) {
+        UserDefaults.standard.removeObject(forKey: kMykey)
+        UserDefaults.standard.synchronize()
+        showAlert(message: "Hemos borrado un valor")
+    }
+    
+    private func showAlert(message: String) {
+        let alert = UIAlertController(title: "My UserDefaults", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
+        present(alert, animated: true, completion: nil)
+    }
+    
 }
 
